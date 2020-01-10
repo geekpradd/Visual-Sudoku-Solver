bd10d83feaad823b701bc2674d860a4b5e3957f5
geekpradd <geekpradd@gmail.com>
2020-01-11 02:01:43 +0530

    add digit recognizer

--- digit_read.py
+++ digit_read.py
@@ -1,13 +1,21 @@
 import cv2
 import numpy as np 
+from neuralnet import Network
 
-img = cv2.imread("sud.jpg")
+img = cv2.imread("ttt.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(img, 5)
-th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 600, 600)
-cv2.imshow("image", th)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+dim = (28, 28)
+resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+l = []
+for a in range(28):
+    for b in range(28):
+        if resized[a,b] <= 127:
+            l.append([1])
+        else:
+            l.append([0])
+
+net = Network([784, 30, 10])
+
+print(np.argmax(net.forward(l)))
