2c04dd0dc5a27bba767adc534d3254096c230483
geekpradd <geekpradd@gmail.com>
2020-01-12 04:25:49 +0530

    Switch to iterative dfs for flood fill in noise cancellation

--- final.py
+++ final.py
@@ -33,6 +33,25 @@ def fillCol(img, i, j, col):
 	img, num4 = fillCol(img, i, j-1, col)
 	return img, 1+num1+num2+num3+num4
 
+def fill_col(img, c_i, c_j, col):
+	# run dfs and fill color
+	stack = [(c_i, c_j)]
+	count = 0
+	while len(stack) != 0:
+		i, j = stack[-1]
+		stack.pop()
+		if i < 0 or i >= img.shape[0] or j < 0 or j >= img.shape[1] or int(img[i][j]) == int(col) or int(img[i][j]) == 0:
+			continue
+		img[i, j] = col
+		stack.append((i+1, j))
+		stack.append((i-1, j))
+		stack.append((i, j+1))
+		stack.append((i, j-1))
+		count+=1
+
+	return img, count
+
+
 def shiftImage(img, i, j) :
 	img2 = np.full(img.shape, 0.0)
 	for a in range(img.shape[0]) :
@@ -44,10 +63,10 @@ def shiftImage(img, i, j) :
 def removeBoundaries(img) :
 	l = img.shape[0]
 	for i in range(l) :
-		img, x = fillCol(img, i, 0, 0)
-		img, x = fillCol(img, 0, i, 0)
-		img, x = fillCol(img, l-i-1, l-1, 0)
-		img, x = fillCol(img, l-1, l-i-1, 0)
+		img, x = fill_col(img, i, 0, 0)
+		img, x = fill_col(img, 0, i, 0)
+		img, x = fill_col(img, l-i-1, l-1, 0)
+		img, x = fill_col(img, l-1, l-i-1, 0)
 	return img
 
 img = cv2.imread('sud5.jpg')
@@ -56,7 +75,7 @@ blur = cv2.GaussianBlur(imgray, (11, 11), 0)
 th = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,5,2)
 kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
-erosion = cv2.erode(th, kernel, iterations = 4)
+erosion = cv2.erode(th, kernel, iterations = 1)
 
 contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 maxA = cv2.contourArea(contours[0], True)
@@ -83,7 +102,10 @@ a3 = np.argmax(DIFF)
 a4 = np.argmin(DIFF)
 
 pts1 = np.float32([[X[a2]+5, Y[a2]+5], [X[a3]-5, Y[a3]+5], [X[a1]-5, Y[a1]-5], [X[a4]+5, Y[a4]-5]])
+print(pts1)
 pts2 = np.float32([[0,0],[306,0],[306,306],[0,306]])
+
+
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
 dst = cv2.warpPerspective(imgray,M,(306,306))
@@ -93,6 +115,9 @@ th_ = np.sum(eh_)/(eh_.size*4)
 ret20, img20 = cv2.threshold(eh_, th_, 255, cv2.THRESH_BINARY_INV)
 
 digits = np.full((9, 9), 0)
+# cv2.imshow("image", img20)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 for i in range(0, 273, 34):
 	for j in range(0, 273, 34):
@@ -100,29 +125,34 @@ for i in range(0, 273, 34):
 		whites = cell2 == 255
 		zs = np.count_nonzero(whites)
 
-		if zs*100.0/cell2.size > 1 :
+		cv2.imshow("image", cell2)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 
-			cell = dst[i+3:i+31, j+3:j+31]
+		if zs*100.0/cell2.size > 1 :
+			cell = dst[i+4:i+32, j+3:j+31]
 			eh = cv2.equalizeHist(cell)
 			th = np.sum(eh)/(eh.size*4)
 			ret, img2 = cv2.threshold(eh, th, 255, cv2.THRESH_BINARY_INV)
 			ar = 0
 			y_m = 0
 			x_m = 0
+
+			
 			for y in range(img2.shape[0]):
 				for x in range(img2.shape[1]):
 					if img2[y][x] == 255:
-						img2, num = fillCol(img2, y, x, 120)
+						img2, num = fill_col(img2, y, x, 120)
 						if num > ar:
 							ar = num
 							y_m = y
 							x_m = x
 
-			img2, num_ = fillCol(img2, y_m, x_m, 255)
+			img2, num_ = fill_col(img2, y_m, x_m, 255)
 			for y in range(img2.shape[0]):
 				for x in range(img2.shape[1]):
 					if img2[y][x] == 120:
-						img2, num = fillCol(img2, y, x, 0)
+						img2, num = fill_col(img2, y, x, 0)
 
 			pps = np.nonzero(img2)
 			X_ = pps[1]
@@ -131,6 +161,10 @@ for i in range(0, 273, 34):
 			xm = (np.min(X_) + np.max(X_))/2
 			rows,cols = img2.shape
 			img2 = shiftImage(img2, int(rows/2-ym), int(cols/2-xm))
+
+			cv2.imshow("image", img2)
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
 			neurons[0] = np.divide(img2[img2 > -1], 255.0)
 			neurons = feedforward(neurons, weights, biases)
 			digits[int(i/34)][int(j/34)] = np.argmax(neurons[num_layers-1])
