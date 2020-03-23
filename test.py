31f0d47c1e34aa9c6d4a4613683d262c6a6a63e2
Harshit Gupta <59705530+HARSHIT-GUPTA-coder@users.noreply.github.com>
2020-03-23 20:32:00 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-from digit_process import test
-import os 
-
-files = os.listdir("test")
-
-total = 0
-hits = 0
-
-for f in files:
-    result = test("test/" + f)
-    total+=1
-    if (result == int(f.split('.')[0][-1:])):
-        hits += 1
-
-    print("Got {0} Was {1}".format(result, f.split('.')[0][-1:]))
-
-print ("Hits {0} Total {1}".format(hits, total))
+import sys
+from sudoku import solve
+s='109800000086507002007006030090050600050020070003060080060100800700603210000005306'
+print(solve(s))
